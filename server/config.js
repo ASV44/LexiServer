4a5b3e2ca9fc967a53db9339552ce6b0e6ea1953
Sasha <vdovicenco.alexandr@gmail.com>
2017-11-06 01:40:58 +0200

    Close #3 performed connection to database and created sample action with it

--- server/config.js
+++ server/config.js
@@ -1,3 +1,8 @@
+let fs = require('fs'),
+configPath = __dirname + '/config.json';
+let parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
+
 export default {
   port: process.env.PORT || 8000,
+  storageConfig: parsed
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import Server from './server'
+
 let app = express()
 
 let server = new Server(app)
